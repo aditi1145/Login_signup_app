b057892ff0b3eb069c2227f780b978f0dbce6ca6
Aditi <119976527+aditi1145@users.noreply.github.com>
2024-10-02 09:44:32 +0530

    Update ProfilePage.jsx

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { json } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
 const ProfilePage = () => {
